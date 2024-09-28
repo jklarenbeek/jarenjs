ff6696de1f1fa053480c0b6250edc9d2e4a0c978
Joham <jklarenbeek@gmail.com>
2024-09-28 21:52:33 +0200

    changed compileschemavalidator to new JarenValidator for string.test.js

--- test/validate/string.test.js
+++ test/validate/string.test.js
@@ -3,77 +3,78 @@ import * as assert from '../assert.node.js';
 
 
 import {
-  compileSchemaValidator,
+  JarenValidator,
 } from '@jarenjs/validate';
 
 // https://json-schema.org/understanding-json-schema/reference/string.html
+const compiler = new JarenValidator()
 
 describe('Schema String Type', function () {
 
   describe('#stringBasic()', function () {
     it('should validate type: \'string\'', function () {
-      const root = compileSchemaValidator({ type: 'string' });
+      const validate = compiler.compile({ type: 'string' });
 
-      assert.isFalse(root.validate(null), 'not validates null');
-      assert.isTrue(root.validate('Déjà vu'), 'validates a string with accents');
-      assert.isTrue(root.validate(''), 'validates an empty string');
-      assert.isFalse(root.validate(42), 'not validates a number');
+      assert.isFalse(validate(null), 'not validates null');
+      assert.isTrue(validate('Déjà vu'), 'validates a string with accents');
+      assert.isTrue(validate(''), 'validates an empty string');
+      assert.isFalse(validate(42), 'not validates a number');
     });
 
     it('should validate type: \'string\', minLength: 2, maxLength: 3,', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         type: 'string',
         minLength: 2,
         maxLength: 3,
       });
 
-      assert.isFalse(root.validate('A'), 'A string is too small');
-      assert.isTrue(root.validate('AB'), 'AB string has valid length');
-      assert.isTrue(root.validate('ABC'), 'ABC string has valid length');
-      assert.isFalse(root.validate('ABCD'), 'ABCD is to long');
+      assert.isFalse(validate('A'), 'A string is too small');
+      assert.isTrue(validate('AB'), 'AB string has valid length');
+      assert.isTrue(validate('ABC'), 'ABC string has valid length');
+      assert.isFalse(validate('ABCD'), 'ABCD is to long');
     });
 
     it('should throw an error on invalid pattern', function () {
-      assert.throws(() => compileSchemaValidator({
+      assert.throws(() => compiler.compile({
         type: 'string',
         pattern: 1234,
       }));
     });
 
     it('should validate type: \'string\', pattern: \'^.*$\'', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         type: 'string',
         pattern: '^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$',
       });
 
-      assert.isTrue(root.validate('555-1212'), '555-1212 is a local phonenumber');
-      assert.isTrue(root.validate('(888)555-1212'), 'is valid phonenumber with area code');
-      assert.isFalse(root.validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
-      assert.isFalse(root.validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
+      assert.isTrue(validate('555-1212'), '555-1212 is a local phonenumber');
+      assert.isTrue(validate('(888)555-1212'), 'is valid phonenumber with area code');
+      assert.isFalse(validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
+      assert.isFalse(validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
     });
 
     it('should validate type: \'string\', pattern: \'/^.*$/i\'', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         type: 'string',
         pattern: '/^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$/',
       });
 
-      assert.isTrue(root.validate('555-1212'), '555-1212 is a local phonenumber');
-      assert.isTrue(root.validate('(888)555-1212'), 'is valid phonenumber with area code');
-      assert.isFalse(root.validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
-      assert.isFalse(root.validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
+      assert.isTrue(validate('555-1212'), '555-1212 is a local phonenumber');
+      assert.isTrue(validate('(888)555-1212'), 'is valid phonenumber with area code');
+      assert.isFalse(validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
+      assert.isFalse(validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
     });
 
     it('should validate type: \'string\', pattern: /^.*$/i', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         type: 'string',
         pattern: /^(\([0-9]{3}\))?[0-9]{3}-[0-9]{4}$/,
       });
 
-      assert.isTrue(root.validate('555-1212'), '555-1212 is a local phonenumber');
-      assert.isTrue(root.validate('(888)555-1212'), 'is valid phonenumber with area code');
-      assert.isFalse(root.validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
-      assert.isFalse(root.validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
+      assert.isTrue(validate('555-1212'), '555-1212 is a local phonenumber');
+      assert.isTrue(validate('(888)555-1212'), 'is valid phonenumber with area code');
+      assert.isFalse(validate('(888)555-1212 ext. 532'), 'is not valid phonenumber with extension');
+      assert.isFalse(validate('(800)FLOWERS'), 'is not valid phonenumber by letters');
 
     });
 
