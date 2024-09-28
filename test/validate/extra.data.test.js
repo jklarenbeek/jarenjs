3008e0b93959b6839eb893f78064c8c8173404f1
Joham <jklarenbeek@gmail.com>
2024-09-28 21:37:16 +0200

    changed compileschemavalidator to new JarenValidator for extra.data.test.js

--- test/validate/extra.data.test.js
+++ test/validate/extra.data.test.js
@@ -2,10 +2,12 @@ import { describe, it } from 'node:test';
 import * as assert from '../assert.node.js';
 
 import {
-  compileSchemaValidator,
+  JarenValidator,
 } from '@jarenjs/validate';
 
 // from: https://json-schema.org/learn/miscellaneous-examples#conditional-validation-with-dependentrequired
+
+const compiler = new JarenValidator();
 
 describe('Schema $data Examples', function () {
 
@@ -20,7 +22,7 @@ describe('Schema $data Examples', function () {
     // The value of "$data" should be a relative JSON - pointer.
 
     it.skip('should validate a value in property smaller is less or equal then larger (not implemented)', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         properties: {
           smaller: {
             type: 'number',
@@ -32,21 +34,21 @@ describe('Schema $data Examples', function () {
         },
       });
 
-      assert.isTrue(root.validate({
+      assert.isTrue(validate({
         smaller: 5,
         larger: 7,
       }));
     });
 
     it.skip('should validate the properties with same format ass their field names (not implemented)', function () {
-      const root = compileSchemaValidator({
+      const validate = compiler.compile({
         additionalProperties: {
           type: 'string',
           format: { $data: '0#' },
         },
       });
 
-      assert.isTrue(root.validate({
+      assert.isTrue(validate({
         'date-time': '1963-06-19T08:30:06.283185Z',
         email: 'joe.bloggs@example.com',
       }));
